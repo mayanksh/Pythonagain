90739257f247dd25e778cc76cbcf987185bf8599
Mayank Sharma <mayank01@mail.fresnostate.edu>
2021-10-02 15:16:30 -0700

    mad libs game

---
+++ venv/leetcode/mad_libs_game.py
@@ -0,0 +1,10 @@
+
+
+color = input("enter the color: ")
+plural_noun = input("enter the plural noun: ")
+celebrity = input("enter the celebrity name: ")
+
+print("roses are " + color)
+print(plural_noun + "are blue")
+print("how about" + celebrity)
+
